dbdc321925ab05564f78c6881c9e4f35d9d6de27
Jon Nordby <jononor@gmail.com>
2025-07-20 22:17:22 +0200

    Looks like CIC2 might also fit 1x1

--- src/cic3_pdm.v
+++ src/cic3_pdm.v
@@ -12,18 +12,18 @@ module cic3_pdm (
 
     // Internal registers
     reg signed [31:0] integrator_0 = 0;
-    //reg signed [31:0] integrator_1 = 0;
+    reg signed [31:0] integrator_1 = 0;
     //reg signed [31:0] integrator_2 = 0;
 
     reg [5:0] decim_counter = 0;
 
     reg signed [31:0] comb_0 = 0;
-    //reg signed [31:0] comb_1 = 0;
+    reg signed [31:0] comb_1 = 0;
     /* verilator lint_off UNUSEDSIGNAL */
     //reg signed [31:0] comb_2 = 0;
 
     reg signed [31:0] delay_0 = 0;
-    //reg signed [31:0] delay_1 = 0;
+    reg signed [31:0] delay_1 = 0;
     //reg signed [31:0] delay_2 = 0;
 
     reg signed [15:0] pcm_out_r = 0;
@@ -33,11 +33,11 @@ module cic3_pdm (
     always @(posedge clk) begin
         if (rst) begin
             integrator_0 <= 0;
-            //integrator_1 <= 0;
+            integrator_1 <= 0;
             //integrator_2 <= 0;
         end else begin
             integrator_0 <= integrator_0 + (pdm_in ? 1 : -1);
-            //integrator_1 <= integrator_1 + integrator_0;
+            integrator_1 <= integrator_1 + integrator_0;
             //integrator_2 <= integrator_2 + integrator_1;
         end
     end
@@ -57,14 +57,14 @@ module cic3_pdm (
             comb_0 <= integrator_0 - delay_0;
             delay_0 <= integrator_0;
 
-            //comb_1 <= comb_0 - delay_1;
-            //delay_1 <= comb_0;
+            comb_1 <= comb_0 - delay_1;
+            delay_1 <= comb_0;
 
             //comb_2 <= comb_1 - delay_2;
             //delay_2 <= comb_1;
 
             // Bit-shift down to get 16-bit output (tune shift based on DECIMATION and stage count)
-            pcm_out_r <= comb_0[OUTPUT_SHIFT + 15 : OUTPUT_SHIFT];
+            pcm_out_r <= comb_1[OUTPUT_SHIFT + 15 : OUTPUT_SHIFT];
             pcm_valid_r <= 1;
         end
     end
