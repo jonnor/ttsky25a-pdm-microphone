fd8b4aa60e2880c8a22a9e5851d450a4c80eda14
Jon Nordby <jononor@gmail.com>
2025-07-20 22:09:32 +0200

    Try CIC1 filter

--- src/cic3_pdm.v
+++ src/cic3_pdm.v
@@ -1,7 +1,6 @@
 
-
 module cic3_pdm (
-    input  wire        clk,        // PDM clock
+    input  wire        clk,        // Main clock
     input  wire        rst,        // active-high synchronous reset
     input  wire        pdm_in,     // 1-bit PDM data input
     output wire signed [15:0] pcm_out, // Decimated PCM output
@@ -12,17 +11,20 @@ module cic3_pdm (
     parameter OUTPUT_SHIFT = 8; // Can tune this
 
     // Internal registers
-    reg signed [24:0] integrator_0 = 0;
-    reg signed [24:0] integrator_1 = 0;
-    reg signed [24:0] integrator_2 = 0;
+    reg signed [31:0] integrator_0 = 0;
+    //reg signed [31:0] integrator_1 = 0;
+    //reg signed [31:0] integrator_2 = 0;
 
     reg [5:0] decim_counter = 0;
-    reg signed [24:0] comb_0 = 0, comb_1 = 0;
 
+    reg signed [31:0] comb_0 = 0;
+    //reg signed [31:0] comb_1 = 0;
     /* verilator lint_off UNUSEDSIGNAL */
-    reg signed [24:0] comb_2 = 0;
+    //reg signed [31:0] comb_2 = 0;
 
-    reg signed [24:0] delay_0 = 0, delay_1 = 0, delay_2 = 0;
+    reg signed [31:0] delay_0 = 0;
+    //reg signed [31:0] delay_1 = 0;
+    //reg signed [31:0] delay_2 = 0;
 
     reg signed [15:0] pcm_out_r = 0;
     reg pcm_valid_r = 0;
@@ -31,12 +33,12 @@ module cic3_pdm (
     always @(posedge clk) begin
         if (rst) begin
             integrator_0 <= 0;
-            integrator_1 <= 0;
-            integrator_2 <= 0;
+            //integrator_1 <= 0;
+            //integrator_2 <= 0;
         end else begin
             integrator_0 <= integrator_0 + (pdm_in ? 1 : -1);
-            integrator_1 <= integrator_1 + integrator_0;
-            integrator_2 <= integrator_2 + integrator_1;
+            //integrator_1 <= integrator_1 + integrator_0;
+            //integrator_2 <= integrator_2 + integrator_1;
         end
     end
 
@@ -52,17 +54,17 @@ module cic3_pdm (
     always @(posedge clk) begin
         pcm_valid_r <= 0;
         if (decim_counter == 63) begin
-            comb_0 <= integrator_2 - delay_0;
-            delay_0 <= integrator_2;
+            comb_0 <= integrator_0 - delay_0;
+            delay_0 <= integrator_0;
 
-            comb_1 <= comb_0 - delay_1;
-            delay_1 <= comb_0;
+            //comb_1 <= comb_0 - delay_1;
+            //delay_1 <= comb_0;
 
-            comb_2 <= comb_1 - delay_2;
-            delay_2 <= comb_1;
+            //comb_2 <= comb_1 - delay_2;
+            //delay_2 <= comb_1;
 
             // Bit-shift down to get 16-bit output (tune shift based on DECIMATION and stage count)
-            pcm_out_r <= comb_2[OUTPUT_SHIFT + 15 : OUTPUT_SHIFT];
+            pcm_out_r <= comb_0[OUTPUT_SHIFT + 15 : OUTPUT_SHIFT];
             pcm_valid_r <= 1;
         end
     end
